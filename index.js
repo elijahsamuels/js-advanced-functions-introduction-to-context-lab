c334c52dc98b882b26343c9425b0693a813f5b70
Elijah Samuels <elijahsamuels@gmail.com>
2021-03-02 16:23:40 -0700

    commit tree 9c4bb016ab8a0bda42f0158e022a7af96f446dab

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Your code here
+// populates a record from an Array
+
+function createEmployeeRecord(array) {
+	return {
+		'firstName': array[0],
+		'familyName': array[1],
+		'title': array[2],
+		'payPerHour': array[3],
+		'timeInEvents': [],
+		'timeOutEvents': []
+	}
+}
+
+// process an Array of Arrays into an Array of employee records
+
+function createEmployeeRecords(arrayOfEmployees) {
+	let employeeRecords = []
+    arrayOfEmployees.forEach((array) => {employeeRecords.push(createEmployeeRecord(array))
+		})
+    return employeeRecords
+}
+
+// familyName,
+// title,
+// payPerHour,
+// timeInEvents,
+// timeOutEvents
+
+
+
+function createTimeInEvent() {
+
+};
+
+function createTimeOutEvent() {
+
+};
+
+function wagesEarnedOnDate() {
+
+};
+
+function allWagesFor() {
+
+};
+
+function calculatePayroll() {
+
+};
+
+function findEmployeeByFirstName() {
+
+};
+
+function calculatePayroll() {
+
+};
